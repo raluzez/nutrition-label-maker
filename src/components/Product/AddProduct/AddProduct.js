9cc48966e816fc5d0b5c438487ff8e4170eeaa7e
Lukas Laurinavicius <lukaslaurina@gmail.com>
2020-01-21 09:23:54 +0200

    add product to recipie fx

--- src/components/Product/AddProduct/AddProduct.js
+++ src/components/Product/AddProduct/AddProduct.js
@@ -1,16 +1,19 @@
 import React from "react";
-import AddProductItem from './AddProductItem/AddProductItem';
+import AddProductItem from "./AddProductItem/AddProductItem";
 import Styles from "./AddProduct.module.css";
 
-const AddProduct = (props) => (
-    <div>
-        <div className={Styles.ProductsTitleLine}>
-            <div>Products</div>
-        </div>
-        {props.products.map( product => (
-            <AddProductItem product={product}/>
-        ))}
+const AddProduct = props => (
+  <div>
+    <div className={Styles.ProductsTitleLine}>
+      <div>Products</div>
     </div>
-)
+    {props.products.map(
+      product =>
+        !props.selectedProducts.find(
+          selectedProduct => selectedProduct.key === product.key
+        ) && <AddProductItem product={product} closeModal={props.closeModal} />
+    )}
+  </div>
+);
 
 export default AddProduct;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,40 +2,53 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import Modal from "../../components/UI/Modal/Modal";
 import AddRecipeModal from "../../components/Recipe/AddRecipeModal/AddRecipeModal";
-import AddProduct from '../../components/Product/AddProduct/AddProduct';
+import AddProduct from "../../components/Product/AddProduct/AddProduct";
 import ItemsList from "../../components/ItemsList/ItemsList";
-import Nutrients from '../../components/Nutrients/Nutrients';
+import Nutrients from "../../components/Nutrients/Nutrients";
 import NutritionLabel from "../../components/NutritionLabel/NutritionLabel";
 import Auth from "../Auth/Auth";
 import * as actions from "../../Store/actions";
 import Styles from "./Home.module.css";
 
-const Home = (props) =>{
-    const [addProductModal, setAddProductModal] = useState(false)
+const Home = props => {
+  const [addProductModal, setAddProductModal] = useState(false);
 
-    const { onFetchProducts, token, userId } = props;
+  const { onFetchProducts, token, userId } = props;
 
-    useEffect(() => {
-      onFetchProducts(token, userId);
-    }, [onFetchProducts, token, userId]);
+  useEffect(() => {
+    onFetchProducts(token, userId);
+  }, [onFetchProducts, token, userId]);
 
-    return(
-            <>  
-                <Modal show={props.showAuthModal}>
-                    <Auth 
-                    switchForm={() => {
-                            props.onCloseSignUp();
-                            setTimeout(()=>{props.onOpenSignUp()},450)}}/>
-                </Modal>
-                <Modal show={addProductModal} closeModal={() => setAddProductModal(false)}>
-                    {console.log(props)}
-                    <AddProduct products={props.products}/>
-                </Modal>
-                <ItemsList  
-                    saveRecipeClicked={() => props.onSaveRecipeClicked(props.recipeAsProduct)}
-                    openAddProductModal={() => setAddProductModal(true)}/>
-                <Nutrients product={props.recipeAsProduct}/>
-                {/* <Modal show={props.showModal}>
+  return (
+    <>
+      <Modal show={props.showAuthModal}>
+        <Auth
+          switchForm={() => {
+            props.onCloseSignUp();
+            setTimeout(() => {
+              props.onOpenSignUp();
+            }, 450);
+          }}
+        />
+      </Modal>
+      <Modal
+        show={addProductModal}
+        closeModal={() => setAddProductModal(false)}
+      >
+        <AddProduct
+          products={props.products}
+          selectedProducts={props.selectedProducts}
+          closeModal={() => setAddProductModal(false)}
+        />
+      </Modal>
+      <ItemsList
+        saveRecipeClicked={() =>
+          props.onSaveRecipeClicked(props.recipeAsProduct)
+        }
+        openAddProductModal={() => setAddProductModal(true)}
+      />
+      <Nutrients product={props.recipeAsProduct} />
+      {/* <Modal show={props.showModal}>
                     <AddRecipeModal
                         recipe={props.recipeAsProduct}
                         closeModal={props.onCloseModal}
@@ -51,30 +64,33 @@ const Home = (props) =>{
                     selectedProducts={props.selectedProducts}
                     saveRecipeClicked={() => props.onSaveRecipeClicked(props.recipeAsProduct)}/>
                 <NutritionLabel product={props.recipeAsProduct}/> */}
-            </>
-)}
+    </>
+  );
+};
 const mapStateToProps = state => {
-    return {
-        recipeAsProduct: state.productList.recipeNutrients,
-        selectedProducts: state.productList.selectedProducts,
-        showModal: state.productList.showModal,
-        showAuthModal: state.auth.showAuthModal,
-        token: state.auth.token,
-        userId: state.auth.userId,
-        products: state.productList.products
-    }
-}
+  return {
+    recipeAsProduct: state.productList.recipeNutrients,
+    selectedProducts: state.productList.selectedProducts,
+    showModal: state.productList.showModal,
+    showAuthModal: state.auth.showAuthModal,
+    token: state.auth.token,
+    userId: state.auth.userId,
+    products: state.productList.products
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-        onSaveRecipeClicked :(product) => dispatch(actions.productClicked(product)),
-        onCloseModal:() => dispatch(actions.closeModal()),
-        onAddRecipeName: (recipeName) => dispatch(actions.addRecipeName(recipeName)),
-        onSaveRecipe: (recipe, items, token, userId) => dispatch(actions.saveRecipe(recipe, items, token, userId)),
-        onCloseSignUp: () => dispatch(actions.closeSignUp()),
-        onOpenSignUp: () => dispatch(actions.openSignUp()),
-        onFetchProducts: (token, userId) => dispatch(actions.fetchProducts(token, userId))
-    }
-}
+  return {
+    onSaveRecipeClicked: product => dispatch(actions.productClicked(product)),
+    onCloseModal: () => dispatch(actions.closeModal()),
+    onAddRecipeName: recipeName => dispatch(actions.addRecipeName(recipeName)),
+    onSaveRecipe: (recipe, items, token, userId) =>
+      dispatch(actions.saveRecipe(recipe, items, token, userId)),
+    onCloseSignUp: () => dispatch(actions.closeSignUp()),
+    onOpenSignUp: () => dispatch(actions.openSignUp()),
+    onFetchProducts: (token, userId) =>
+      dispatch(actions.fetchProducts(token, userId))
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
