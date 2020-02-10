52fe4c683d3fd457166d1111bd00360716939606
Lukas Laurinavicius <lukaslaurina@gmail.com>
2020-02-11 00:19:09 +0200

    firebase

---
+++ build/precache-manifest.00be1076a1deaf77252b368b034abdbb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57315c8e873a337aa700dacb320ee0cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5c6831694975a6b60fc",
+    "url": "/static/css/main.70b42922.chunk.css"
+  },
+  {
+    "revision": "214a35a45786a3993c6a",
+    "url": "/static/js/2.9049f796.chunk.js"
+  },
+  {
+    "revision": "8c6047655a068b17d1266ea8fed28f18",
+    "url": "/static/js/2.9049f796.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5c6831694975a6b60fc",
+    "url": "/static/js/main.f7ba3bdb.chunk.js"
+  },
+  {
+    "revision": "75c6f9419ce28d30d7fb",
+    "url": "/static/js/runtime-main.97d8cb49.js"
+  },
+  {
+    "revision": "d1e45465c740e95c624e3bcce0ca11f6",
+    "url": "/static/media/Google_icon.d1e45465.png"
+  },
+  {
+    "revision": "e4172b1554ff6ba8e0a5e1755497b286",
+    "url": "/static/media/Nutrients.e4172b15.png"
+  },
+  {
+    "revision": "54042046b942f8e2a4d830bcce80799e",
+    "url": "/static/media/NutritionLabel.54042046.png"
+  },
+  {
+    "revision": "3791c5c9db620bafcdb6735585e67315",
+    "url": "/static/media/Recipe.3791c5c9.png"
+  },
+  {
+    "revision": "9bdcf6419f51a09a8c835cd85be0014e",
+    "url": "/static/media/landingPage.9bdcf641.jpg"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import firebase from "firebase";
 import "./index.css";
@@ -12,18 +12,13 @@ import productListReducer from "./Store/reducers/productsList";
 import recipeReducer from "./Store/reducers/recipe";
 import authReducer from "./Store/reducers/auth";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const rootReducer = combineReducers({
   productList: productListReducer,
   recipe: recipeReducer,
   auth: authReducer
 });
 
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
+const store = createStore(rootReducer ,applyMiddleware(thunk));
 
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
